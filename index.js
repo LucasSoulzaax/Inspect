f3afb07ee039b832f45dc9fe5b4935ee5bda7666
Lucas Souza <Lucasnayra1509@gmail.com>
2025-05-04 13:19:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const cors = require('cors')
+const pool = require('./db.js')
+const PORT = 3000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/produtos', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM produtos')
+    res.json(rows)
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Falha ao conectar no banco', error: error.message })
+  }
+})
+
+app.post('/produtos', async (req, res) => {
+  const { nome, descricao, validade, status } = req.body
+
+  try {
+    const consulta =
+      'INSERT INTO produtos (nome, descricao, validade, status) VALUES ($1, $2, $3, $4)' 
+
+    await pool.query(consulta, [nome, descricao, validade, status])
+
+    res.status(201).json({ message: 'produto cadastrado com sucesso' })
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Falha ao cadastrar produto', error: error.message })
+  }
+})
+
+app.put('/produtos/:id', async (req, res) => {
+  const { id } = req.params
+  const {status } = req.body
+
+  try {
+    const consulta =
+      'UPDATE usuarios SET status = $1 WHERE id = $2'
+
+    await pool.query(consulta, [status, id])
+    res.status(200).json({ message: 'produto atualizado com sucesso' })
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Falha ao atualizar produto', error: error.message })
+  }
+})
+
+app.delete('/produtos/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const consulta = 'DELETE FROM produtos WHERE id = $1' // Correção aqui
+
+    await pool.query(consulta, [id])
+    res.status(200).json({ message: 'produto deletado com sucesso' })
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Falha ao deletar produto', error: error.message })
+  }
+})
+
+
+app.post('/inspetor', async (req, res) => {
+  const { produto_id, justificativa } = req.body
+
+  if (!produto_id || !justificativa) {
+    return res.status(400).json({ message: 'produto_id e justificativa são obrigatórios' })
+  }
+
+  try {
+    const consulta = 'INSERT INTO inspetor (produto_id, justificativa) VALUES ($1, $2)'
+    await pool.query(consulta, [produto_id, justificativa])
+    res.status(201).json({ message: 'Justificativa registrada com sucesso' })
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao registrar justificativa', error: error.message })
+  }
+})
+app.listen(PORT, () => {
+  console.log('API está no AR')
+})
+
+
